ba8120d28905f4d13e4cfee89fa33786c4831832
JasonZhangCauc <hasp_Jason@163.com>
2018-09-15 13:24:19 +0800

    Signed-off-by: JasonZhangCauc <hasp_Jason@163.com>

--- JZOffer/src/Test01.java
+++ JZOffer/src/Test01.java
@@ -4,9 +4,22 @@
  */
 public class Test01 {
 	
+	public static boolean Find(int target, int[][] array) {
+		for (int i = (array.length - 1); i >= 0; i--) {
+			for (int j = 0; j < array[i].length; j++) {
+				if (array[i][j] == target)
+					return true;
+				if (array[i][j] > target)
+					break;
+			}
+		}
+		return false;
+	}
+	//²âÊÔ
 	public static void main(String[] args) {
-		
-
+		int target=5;
+		int [][]array= {{1,2,8,9},{2,4,9,12},{4,7,10,13},{6,8,11,15}};
+		System.out.println(Find(target,array));
 	}
 
 }
