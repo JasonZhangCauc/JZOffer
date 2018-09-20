ac99793fc3585847a901b600a5542072c14aeb12
JasonZhangCauc <hasp_Jason@163.com>
2018-09-20 16:21:45 +0800

    Signed-off-by: JasonZhangCauc <hasp_Jason@163.com>

---
+++ JZOffer/src/Test15.java
@@ -0,0 +1,37 @@
+
+/**
+ * 
+ * 输入一个链表，反转链表后，输出新链表的表头。
+ *
+ */
+public class Test15 {
+    public ListNode ReverseList(ListNode head) {
+		if(head==null) {
+			return null;
+		}
+    	ListNode new_node=null;
+    	ListNode tmp=null;
+    	ListNode pre=null;
+    	while(head!=null) {
+    		tmp=head.next;
+    		head.next=pre;
+    		if(tmp==null) 
+    			new_node=head;
+    		pre=head;
+    		head=tmp;
+    	}
+    	
+    	return new_node;
+
+    }
+    //题自带
+    public class ListNode {
+        int val;
+        ListNode next = null;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+}
+
