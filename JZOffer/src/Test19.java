afff2663ed0e390be2f89d454f0051456288fadc
JasonZhangCauc <hasp_Jason@163.com>
2018-10-05 21:58:18 +0800

    顺时针打印矩阵
    Signed-off-by: JasonZhangCauc <hasp_Jason@163.com>

--- JZOffer/src/Test19.java
+++ JZOffer/src/Test19.java
@@ -46,7 +46,6 @@ public class Test19 {
 		}
 
 		System.out.print(arrayList.toString());
-
 		return arrayList;
 
 	}
@@ -75,3 +74,24 @@ public class Test19 {
 		return arrayList;
 	}
 }
+//其他方法
+/**
+import java.util.ArrayList;
+public class Solution {
+    public ArrayList<Integer> printMatrix(int [][] array) {
+        ArrayList<Integer> result = new ArrayList<Integer> ();
+        if(array.length==0) return result;
+        int n = array.length,m = array[0].length;
+        if(m==0) return result;
+        int layers = (Math.min(n,m)-1)/2+1;//这个是层数
+        for(int i=0;i<layers;i++){
+            for(int k = i;k<m-i;k++) result.add(array[i][k]);//左至右
+            for(int j=i+1;j<n-i;j++) result.add(array[j][m-i-1]);//右上至右下
+            for(int k=m-i-2;(k>=i)&&(n-i-1!=i);k--) result.add(array[n-i-1][k]);//右至左
+            for(int j=n-i-2;(j>i)&&(m-i-1!=i);j--) result.add(array[j][i]);//左下至左上
+        }
+        return result;      
+    }
+}
+
+*/
